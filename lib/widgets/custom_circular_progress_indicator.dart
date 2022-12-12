643940a5e04b8127cdb7c283c50f019f0a52ecde
Tamas Nemes <nemestomi2@gmail.com>
2022-12-12 12:05:24 +0100

    Update progress indicator animation size

--- lib/widgets/custom_circular_progress_indicator.dart
+++ lib/widgets/custom_circular_progress_indicator.dart
@@ -8,7 +8,7 @@ class CustomCircularProgressIndicator extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Center(
       child: SizedBox(
-        height: 100,
+        height: 150,
         child: RiveAnimation.asset(
           "assets/animations/circular_progress_indicator.riv",
           fit: BoxFit.contain,
