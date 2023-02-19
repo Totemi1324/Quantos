16b79fcd3b9b543b879dc53463b77f587c41328b
Totemi1324 <nemestomi2@gmail.com>
2023-02-20 00:20:21 +0100

    Finalize difficulty localization

--- lib/bloc/stores/difficulty_store_service.dart
+++ lib/bloc/stores/difficulty_store_service.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import './store_service.dart';
-import '../../';
+import '../../models/lection.dart';
 
 class DifficultyStoreService extends StoreService<Map<Difficulty, String>> {
   static final Map<Difficulty, String> _default = {
@@ -12,9 +12,9 @@ class DifficultyStoreService extends StoreService<Map<Difficulty, String>> {
   };
 
   DifficultyStoreService(Locale currentLocale) : super(_default, currentLocale);
-  
+
   @override
-  Map<int, String> getLocalizedStore(AppLocalizations localization) {
+  Map<Difficulty, String> getLocalizedStore(AppLocalizations localization) {
     final Map<Difficulty, String> localizedStore = {
       Difficulty.easy: localization.lectionScreenDifficultyClass1,
       Difficulty.advanced: localization.lectionScreenDifficultyClass2,
