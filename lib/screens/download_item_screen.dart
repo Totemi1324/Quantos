337c6eba48508afd9cfe32eaaf267031183589bb
Totemi1324 <nemestomi2@gmail.com>
2023-04-08 23:32:28 +0200

    Insert download button animation

--- lib/screens/download_item_screen.dart
+++ lib/screens/download_item_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen/assets.gen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/download_service.dart';
@@ -42,112 +43,118 @@ class _DownloadItemScreenState extends State<DownloadItemScreen> {
         ? const ErrorPageContent()
         : Decorated(
             showSettings: false,
-            body: SingleChildScrollView(
-              child: SafeArea(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    PanelCard(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30, vertical: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            download.title,
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.headlineMedium,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  download.size
-                                      .convertToLocalizedString(context),
+            body: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  PanelCard(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          download.title,
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.headlineMedium,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                download.size.convertToLocalizedString(context),
+                                style: Theme.of(context).textTheme.bodySmall,
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                child: Text(
+                                  "•",
                                   style: Theme.of(context).textTheme.bodySmall,
                                 ),
-                                Container(
-                                  margin: const EdgeInsets.symmetric(
-                                      horizontal: 20),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(right: 10),
+                                child: Icon(
+                                  widget.iconForFileType[download.type],
+                                ),
+                              ),
+                              Text(
+                                download.type.name.toUpperCase(),
+                                style: Theme.of(context).textTheme.bodySmall,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          download.description,
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 10, top: 25),
+                          width: double.infinity,
+                          child: Text(
+                            AppLocalizations.of(context)!
+                                .downloadSelectLocaleTitle,
+                            style: Theme.of(context).textTheme.bodySmall,
+                          ),
+                        ),
+                        GridView.extent(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          maxCrossAxisExtent: 100,
+                          childAspectRatio: 1.5,
+                          crossAxisSpacing: 10,
+                          children: download.locales
+                              .map<Widget>(
+                                (locale) => OutlinedButton(
+                                  onPressed: () => setState(() {
+                                    _selected = locale;
+                                  }),
+                                  style: ButtonStyle(
+                                    side: MaterialStateProperty.all(
+                                      BorderSide(
+                                        color: locale == _selected
+                                            ? Theme.of(context)
+                                                .colorScheme
+                                                .primary
+                                            : Colors.white.withOpacity(0.3),
+                                      ),
+                                    ),
+                                    shape: MaterialStateProperty.all(
+                                      RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                    ),
+                                  ),
                                   child: Text(
-                                    "•",
+                                    locale,
                                     style:
-                                        Theme.of(context).textTheme.bodySmall,
+                                        Theme.of(context).textTheme.labelMedium,
                                   ),
                                 ),
-                                Container(
-                                  margin: const EdgeInsets.only(right: 10),
-                                  child: Icon(
-                                    widget.iconForFileType[download.type],
-                                  ),
-                                ),
-                                Text(
-                                  download.type.name.toUpperCase(),
-                                  style: Theme.of(context).textTheme.bodySmall,
-                                ),
-                              ],
-                            ),
-                          ),
-                          Text(
-                            download.description,
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.labelSmall,
-                          ),
-                          Container(
-                            margin: const EdgeInsets.only(bottom: 10, top: 25),
-                            width: double.infinity,
-                            child: Text(
-                              AppLocalizations.of(context)!
-                                  .downloadSelectLocaleTitle,
-                              style: Theme.of(context).textTheme.bodySmall,
-                            ),
-                          ),
-                          GridView.extent(
-                            physics: const NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            maxCrossAxisExtent: 100,
-                            childAspectRatio: 1.5,
-                            crossAxisSpacing: 10,
-                            children: download.locales
-                                .map<Widget>(
-                                  (locale) => OutlinedButton(
-                                    onPressed: () => setState(() {
-                                      _selected = locale;
-                                    }),
-                                    style: ButtonStyle(
-                                      side: MaterialStateProperty.all(
-                                        BorderSide(
-                                          color: locale == _selected
-                                              ? Theme.of(context)
-                                                  .colorScheme
-                                                  .primary
-                                              : Colors.white.withOpacity(0.3),
-                                        ),
-                                      ),
-                                      shape: MaterialStateProperty.all(
-                                        RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                        ),
-                                      ),
-                                    ),
-                                    child: Text(
-                                      locale,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .labelMedium,
-                                    ),
-                                  ),
-                                )
-                                .toList(),
-                          )
-                        ],
+                              )
+                              .toList(),
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: SizedBox(
+                        height: 200,
+                        child: RiveAnimation.asset(
+                          Assets.animations.downloadButton,
+                          fit: BoxFit.contain,
+                          stateMachines: const ["ListenForPress"],
+                        ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           );
