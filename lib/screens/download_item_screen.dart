3d6a4a5a23c2c51a8b1d088cfcaae058e50d2d14
Totemi1324 <nemestomi2@gmail.com>
2023-04-27 21:42:13 +0200

    Restrict downlad functionality to web (preview)

--- lib/screens/download_item_screen.dart
+++ lib/screens/download_item_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_gen/gen/assets.gen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 import '../bloc/download_service.dart';
 
@@ -158,7 +159,9 @@ class _DownloadItemScreenState extends State<DownloadItemScreen> {
                             final downloadLink = Uri.tryParse(
                               download.linkForLocale(_selected ?? "") ?? "",
                             );
-                            if (downloadLink != null && await canLaunchUrl(downloadLink)) {
+                            if (UniversalPlatform.isWeb &&
+                                downloadLink != null &&
+                                await canLaunchUrl(downloadLink)) {
                               await launchUrl(downloadLink);
                             }
                           },
