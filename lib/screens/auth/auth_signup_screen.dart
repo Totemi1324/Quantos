c5b33685398ca4ede7244cc2db3e77103cc41090
Tamas Nemes <nemestomi2@gmail.com>
2023-05-23 23:05:27 +0200

    Configure new signup screen layout

--- lib/screens/auth/auth_signup_screen.dart
+++ lib/screens/auth/auth_signup_screen.dart
@@ -2,18 +2,40 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_gen/gen/assets.gen.dart';
 import 'package:quantos/widgets/forms/signup_submit.dart';
+import 'package:rive/rive.dart';
 
 import '../base/flat.dart';
 import '../../widgets/forms/signup_form.dart';
 
-class AuthSignUpScreen extends StatelessWidget {
+class AuthSignUpScreen extends StatefulWidget {
   static const routeName = "/authenticate/sign-up";
-  final formKey = GlobalKey<FormState>();
-  final emailController = StreamController<String>();
-  final passwordController = StreamController<String>();
 
-  AuthSignUpScreen({super.key});
+  const AuthSignUpScreen({super.key});
+
+  @override
+  State<AuthSignUpScreen> createState() => _AuthSignUpScreenState();
+}
+
+class _AuthSignUpScreenState extends State<AuthSignUpScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = StreamController<String>();
+  final _passwordController = StreamController<String>();
+
+  SMITrigger? _passwordTyping;
+  SMITrigger? _formValid;
+  SMITrigger? _formInvalid;
+
+  void _onInit(Artboard artboard) {
+    var controller =
+        StateMachineController.fromArtboard(artboard, "PasswordStates")
+            as StateMachineController;
+    artboard.addController(controller);
+    _passwordTyping = controller.findInput<bool>("passwordTyping") as SMITrigger;
+    _formValid = controller.findInput<bool>("formInvalid") as SMITrigger;
+    _formInvalid = controller.findInput<bool>("formInvalid") as SMITrigger;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,33 +45,42 @@ class AuthSignUpScreen extends StatelessWidget {
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 30),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    margin: const EdgeInsets.only(top: 50),
-                    child: Text(
-                      AppLocalizations.of(context)!.authSignUpScreenTitle,
-                      style: Theme.of(context).textTheme.headlineLarge,
-                    ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(top: 50),
+                  child: Text(
+                    AppLocalizations.of(context)!.authSignUpScreenTitle,
+                    style: Theme.of(context).textTheme.headlineLarge,
                   ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(vertical: 50),
-                    child: SignUpForm(
-                      formKey,
-                      onEmailSave: (newEmail) => emailController.add(newEmail),
-                      onPasswordSave: (newPassword) => passwordController.add(newPassword),
-                    ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 50),
+                  child: SignUpForm(
+                    _formKey,
+                    onEmailSave: (newEmail) => _emailController.add(newEmail),
+                    onPasswordSave: (newPassword) =>
+                        _passwordController.add(newPassword),
                   ),
-                  SignupSubmit(
-                    formKey,
-                    emailStream: emailController.stream,
-                    passwordStream: passwordController.stream,
+                ),
+                Expanded(
+                  child: RiveAnimation.asset(
+                    Assets.animations.passwordAssistant,
+                    fit: BoxFit.contain,
+                    onInit: _onInit,
                   ),
-                ],
-              ),
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(vertical: 30),
+                  child: SignupSubmit(
+                    _formKey,
+                    emailStream: _emailController.stream,
+                    passwordStream: _passwordController.stream,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
